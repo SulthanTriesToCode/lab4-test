1e15537af2ea30dacda5d3381853264665fa2b02
Bruh <sulthanprabowo7@gmail.com>
2024-08-28 16:34:09 +1000

    modify eslint config and eslintrc again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { 
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+      ...globals.commonjs,
+      ...globals.es6,
+      ...globals.jest,
+    },
+  }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
